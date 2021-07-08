37ba79768e4f3a72f21790e7fe04d929c00503ca
luke <luke.ichpekov@gmail.com>
2021-07-08 13:42:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,26 +8,26 @@ require('dotenv').config()
 let riotRes
 let selectedRegion
 let randomVar
-let app = express()
+let app = express() //declares an express application
 let apiKey = process.env.API_KEY
-let port = process.env.PORT || 3000 
-app.use(express.static('public'))
-app.use(express.json())
-app.listen(port, () => console.log(`listening at port ${port}`))
+let port = process.env.PORT || 3000  //sets the port
+app.use(express.static('public')) //allows express to get access to other files in project
+app.use(express.json()) //allows for manipulation of json files
+app.listen(port, () => console.log(`listening at port ${port}`)) //starts listening at a port (turns on backend)
 let url 
 let cors = require('cors')
-const { callbackify } = require('util')
-const { parse } = require('path')
+const { callbackify } = require('util') //importing stuff to debug later
+const { parse } = require('path') //deal with paths
 //const { response } = require('express')
 //const { json } = require('express')
-app.use(cors())
+app.use(cors()) //dealing with cors errors from API and front end
 app.use(bodyParser.json())
 app.options('*', cors())
-headers: {"Access-Control-Allow-Origin", "*"}
+headers: {"Access-Control-Allow-Origin", "*"} //makes sure we dont get cors errors
 
 
 
-app.get('/', function(req, res)  {
+app.get('/', function(req, res)  { //test to make sure server is on
   res.write('SERVER PAGE')
   res.end()
 })
@@ -37,11 +37,11 @@ app.get('/', function(req, res)  {
 
 //searchbar post
 app.post('/userInput', (req, res) => {
-let summonerName = req.body.userInput
-let summonerRegion = req.body.regionCode
-summonerURL = `https://${summonerRegion}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`
-getSummonerID(summonerURL)
-app.use (function(req, res, next ) {
+let summonerName = req.body.userInput //gets name from the input in the search bar
+let summonerRegion = req.body.regionCode //gets region from the user selected region
+summonerURL = `https://${summonerRegion}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}` //gets the url for the api we will call
+getSummonerID(summonerURL) //calls function to get id
+app.use (function(req, res, next ) { //dealing with cors errors
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Origin', 'GET, POST, DELETE, PUT')
   res.header('Access-Control-Allow-Origin', 'Content-Type')
@@ -50,22 +50,21 @@ app.use (function(req, res, next ) {
 
 
 
-//get summoner account shit, like ID and account NUM ect 
+//get summoner account, ID and account NUM ect 
 function getSummonerID(summonerURL){
-  https
-  .get(summonerURL, (resp) => {
-    let data =''
+  https.get(summonerURL, (resp) => {
+    let data ='' //declare empty string to populate with incoming data
 
     resp.on('data', (chunk) => {
-      data += chunk
+      data += chunk //gets api response in chunks, and adds it to empty string
     })
     resp.on("end", () => {
       let initialResponse = data
-      parsedInitialResponse = JSON.parse(initialResponse)
-      let sumLevel =  parsedInitialResponse.summonerLevel
-      let IdNum = parsedInitialResponse.id
-      let sumName = parsedInitialResponse.name
-      let accouuntId = parsedInitialResponse.accountId
+      parsedInitialResponse = JSON.parse(initialResponse) //parses the json response
+      let sumLevel =  parsedInitialResponse.summonerLevel //gets the summoner level from the response
+      let IdNum = parsedInitialResponse.id //gets the summoner id number from response
+      let sumName = parsedInitialResponse.name //gets the account name from the response
+      let accouuntId = parsedInitialResponse.accountId //gets accound id from response
       console.log(`summoner level is ${sumLevel}`)
       console.log(`id number for summoner is ${IdNum}`)
       console.log(`Summoner Name Is ${sumName}`)
@@ -81,13 +80,12 @@ function getSummonerID(summonerURL){
 )}
 
 
-//get summoner WIN LOSS, rank that stuff
+//get summoner WIN LOSS, rank
 function callLeagueV4(Id, sumLevel, sumName, accountId){
   let leagueV4URL = `https://${summonerRegion}.api.riotgames.com/lol/league/v4/entries/by-summoner/${Id}?api_key=${apiKey}`
-  https
-  .get(leagueV4URL, (resp) => {
+  https.get(leagueV4URL, (resp) => {
     let data =''
-
+//calls api again
     resp.on('data', (chunk) => {
       data += chunk
     })
@@ -97,7 +95,7 @@ function callLeagueV4(Id, sumLevel, sumName, accountId){
     })
   })
   .on("error", err => {
-    console.log("big poo poo " + err.message )
+    console.log("error occured " + err.message )
 }
 )}
 
@@ -105,8 +103,7 @@ function callLeagueV4(Id, sumLevel, sumName, accountId){
 //get Summoner last 100 RANKED games
 function callMatchV4(Id, sumLevel, sumName, accountId, leagueV4Response){
   let matchV4URL = `https://${summonerRegion}.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?queue=420&api_key=${apiKey}`
-  https
-  .get(matchV4URL, (resp) => {
+  https.get(matchV4URL, (resp) => {
     let data =''
 
     resp.on('data', (chunk) => {
@@ -114,9 +111,9 @@ function callMatchV4(Id, sumLevel, sumName, accountId, leagueV4Response){
     })
     resp.on("end", () => {
       let matchV4Response = data
-      let JSONSumLevel= JSON.stringify(sumLevel)
-      let JSONSumName = JSON.stringify(sumName)
-
+      let JSONSumLevel= JSON.stringify(sumLevel) //converts level to json string
+      let JSONSumName = JSON.stringify(sumName) //converts summoner name to json string 
+  //sends json object to front end with user in-game information.
       res.json({
         matchV4Response,
         leagueV4Response,
@@ -127,7 +124,7 @@ function callMatchV4(Id, sumLevel, sumName, accountId, leagueV4Response){
     })
   })
   .on("error", err => {
-    console.log("big poo poo " + err.message )
+    console.log("error occured callMatchV4 " + err.message )
 }
 )}
 
@@ -136,12 +133,12 @@ function callMatchV4(Id, sumLevel, sumName, accountId, leagueV4Response){
 
 
 
-app.post('/', (req, res)  => {
+app.post('/', (req, res)  => { //post request from frontend
   selectedRegion = req.body.regionCode 
   url = `https://${selectedRegion}.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/CHALLENGER/I?page=1&api_key=${apiKey}`
   callRiot(url)
   
- function callRiot(url) {  
+ function callRiot(url) {  //get more data from api
 https
  .get(url, (resp) => {
      let data = ''
@@ -159,7 +156,7 @@ https
 
 })
   .on("error", err => {
-    console.log("big Mistake lul " + err.message )
+    console.log("error message sending back data " + err.message )
 })
 
 
@@ -167,7 +164,7 @@ https
 
 }
 
-function getValue(value) {
+function getValue(value) { //sends data for challenger players to front end 
   this.value = value
   riotData = this.value
   res.json({
